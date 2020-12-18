0942ff37b1bccddab224a151060fc0aa100a0a3e
hyfsy <1577975140@qq.com>
2020-12-18 22:23:05 +0800

    修改枚举单例的实现

--- src/designpattern/singleton/EnumSingleton.java
+++ src/designpattern/singleton/EnumSingleton.java
@@ -2,15 +2,30 @@ package designpattern.singleton;
 
 /**
  * 枚举单例
- *
- * 调用：EnumSingleton.SINGLETON.func();
+ * <p>
+ * 调用：EnumSingleton.getInstance();
  */
-public enum EnumSingleton {
+public class EnumSingleton {
 
-    //单例对象
-    SINGLETON;
+    private EnumSingleton() {
+    }
 
-    public void func(){
+    public static EnumSingleton getInstance() {
+        return Singleton.INSTANCE.getInstance();
+    }
 
+    private enum Singleton {
+        INSTANCE;
+
+        private EnumSingleton instance;
+
+        // 构造函数只会被装载一次
+        Singleton() {
+            instance = new EnumSingleton();
+        }
+
+        public EnumSingleton getInstance() {
+            return instance;
+        }
     }
 }
